9c50fcc6bcbfa02ef754e0932e4962a8e76cdc39
gorgonz <gorgonz11@gmail.com>
2017-06-06 14:47:18 +0200

    Create plgContent.php
    Add an example class plgContent

---
+++ plgContent.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *  Lets have an independant mock class
+ */
+ class plgContent
+ {
+ 
+
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+
+	 /**
+	* Content string Definition
+	* Scan for module string
+	*/
+	public function onContentPrepare($context, &$article, &$params, $page) 
+  {
+		return true;
+	}
+ 
+ }
+
+?>
